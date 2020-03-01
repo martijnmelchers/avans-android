e81de9b907c47c182015397f752c1cfd7a6cac5d
SaschaMendel <saschamendel2016@gmail.com>
2020-03-01 12:34:59 +0100

    uri added to locallocation

--- app/src/main/java/com/example/weatherinator/models/LocalLocation.java
+++ app/src/main/java/com/example/weatherinator/models/LocalLocation.java
@@ -1,25 +1,33 @@
 package com.example.weatherinator.models;
 
+import android.net.Uri;
+
 public class LocalLocation {
     private String name;
     private WeatherLocation weather = null;
+    private Uri imageSource = null;
 
     public LocalLocation(String name){
         this.name = name;
     }
-
     public String GetName(){
         return this.name;
     }
-
     public WeatherLocation GetWeatherLocation() {
         return this.weather;
     }
-
     public void setWeather(WeatherLocation weather){
         this.weather = weather;
     }
     public boolean HasWeather(){
         return this.weather != null;
+    }
+
+    public Uri GetImageSource(){
+        return this.imageSource;
+    }
+    
+    private void SetImageSource(Uri imageSource){
+        this.imageSource = imageSource;
     }
 }
